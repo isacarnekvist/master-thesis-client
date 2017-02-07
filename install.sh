0308bcd4a3bbe9ee00edeecb02cac6f363b0b657
Isac Arnekvist <isac.arnekvist@gmail.com>
2017-02-07 08:36:47 +0100

    further install things

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+sudo apt-get install python3-pip python3-scipy -y
 sudo pip3 install keras
 sudo pip3 install theano
+sudo pip3 install pyuarm
 mkdir -p $HOME/.keras
 echo '{"image_dim_ordering": "tf", "epsilon": 1e-07, "floatx": "float32", "backend": "theano"}' > $HOME/.keras/keras.json
+git clone git@github.com:isacarnekvist/naf.git
