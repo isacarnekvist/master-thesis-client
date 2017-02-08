c992401b23b3f2cbd0e0c547cec1122ac1204736
Isac Arnekvist <isac.arnekvist@gmail.com>
2017-02-08 08:36:10 +0100

    hopefully stability improvements

--- client.py
+++ client.py
@@ -29,17 +29,23 @@ def random_in_range(a, b):
 def is_lose_pose(x, y, z):
     if not (-0.12 <= x <= 0.12):
         return True
-    if not (0.12 <= y <= 0.27):
+    if not (0.15 <= y <= 0.27):
         return True
     if not (0.025 <= z <= 0.038):
         return True
     return False
 
+def is_win_pose(x, y, z, gx, gy):
+    if euclidean([x, y], [gx, gy]) < 0.01:
+        return True
+    else:
+        return False
+
 
 def random_pose():
     return (
-        random_in_range(-0.10, 0.10),
-        random_in_range(0.15, 0.25),
+        random_in_range(-0.12, 0.12),
+        random_in_range(0.17, 0.25),
         random_in_range(0.029, 0.034)
     )
 
@@ -61,11 +67,16 @@ class Client():
         self.arm = Arm()
         self.nn = NNet(x_size=3 + 2, u_size=3)
         self.session = requests.Session()
+        self.max_angle_change = 3.8
         sleep(2.0)
 
     def update_weights(self):
         logger.debug('Fetching new parameters')
-        params = json.loads(self.session.get('http://beorn:5000/get_params').text)
+        try:
+            params = json.loads(self.session.get('http://beorn:5000/get_params').text)
+        except requests.exceptions.ConnectionError:
+            logger.error('Server not online? Could not update weights.')
+            return
         self.nn.q.set_weights([np.array(param) for param in params])
 
     def start(self):
@@ -74,38 +85,45 @@ class Client():
     def random_start_pose(self):
         x, y, z = random_pose()
         self.arm.move_to(x, y, z + 0.015, velocity=0.5)
-        sleep(2.0)
+        sleep(4.0)
         self.arm.move_to(x, y, z)
-        sleep(0.3)
+        sleep(2.0)
 
     def next_move(self, noise_factor=1.0):
         eef_x, eef_y, eef_z = self.arm.get_position()
 
         # new controls plus noise
-        u_alpha, u_beta, u_gamma = 5.0 * self.nn.mu.predict(create_state_vector(
+        u_alpha, u_beta, u_gamma = self.max_angle_change * self.nn.mu.predict(create_state_vector(
             eef_x, eef_y, eef_z, self.goal_x, self.goal_y
         ))[0, :] + noise_factor * 1.0 * np.random.randn(3)
 
-        if abs(u_alpha) > 6.0:
-            u_alpha = 6.0 * np.sign(u_alpha)
-        if abs(u_beta) > 6.0:
-            u_beta = 6.0 * np.sign(u_beta)
-        if abs(u_gamma) > 6.0:
-            u_gamma = 6.0 * np.sign(u_gamma)
+        if abs(u_alpha) > self.max_angle_change:
+            u_alpha = self.max_angle_change * np.sign(u_alpha)
+        if abs(u_beta) > self.max_angle_change:
+            u_beta = self.max_angle_change * np.sign(u_beta)
+        if abs(u_gamma) > self.max_angle_change:
+            u_gamma = self.max_angle_change * np.sign(u_gamma)
 
         return u_alpha, u_beta, u_gamma
 
     def control_loop(self):
         experience = []
+        self.update_weights()
         latest_param_update = datetime.now()
         while True:
             logger.debug('Setting random start and goal poses')
             self.random_start_pose()
             self.goal_x, self.goal_y, _ = random_pose()
-            for j in range(10):
+            while True:
                 x, y, z = self.arm.get_position()
                 u = self.next_move()
-                self.arm.set_angles_relative(*self.next_move())
+                logger.debug('Sending angles: {}'.format(u))
+                if self.arm._arm.is_connected():
+                    self.arm.set_angles_relative(*self.next_move())
+                else:
+                    self.arm.disconnect()
+                    self.arm = Arm()
+                    break
                 sleep(0.1)
                 xp, yp, zp = self.arm.get_position()
                 state_prime = create_state_vector(xp, yp, zp, self.goal_x, self.goal_y)
@@ -123,7 +141,6 @@ class Client():
                 if is_lose_pose(xp, yp, zp):
                     logger.debug('outside workspace, reward -2')
                     break
-                sleep(0.1)
             if datetime.now() > latest_param_update + timedelta(seconds=15):
                 self.update_weights()
                 latest_param_update = datetime.now()
@@ -133,8 +150,11 @@ class Client():
 
     def send_experience(self, experience):
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = self.session.put('http://beorn:5000/put_experience', data=json.dumps(experience), headers=headers)
-        logger.debug('Sent experience {}'.format(r))
+        try:
+            r = self.session.put('http://beorn:5000/put_experience', data=json.dumps(experience), headers=headers)
+            logger.debug('Sent experience {}'.format(r))
+        except requests.exceptions.ConnectionError:
+            logger.error('Server not online? Could not send experience.')
 
     def stop(self):
         self.arm.stop()
