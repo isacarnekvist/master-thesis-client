d57a3ba732f3ee7c0503de88fe6f17ae6fb4ab43
Isac Arnekvist <isac.arnekvist@gmail.com>
2017-03-06 15:57:49 +0100

    a3c-standalone with simulation bug fixed

--- a3c-standalone.py
+++ a3c-standalone.py
@@ -1,7 +1,15 @@
-from __future__ import print_function
+import json
+import multiprocessing
+from time import sleep
+from queue import Full
+from datetime import datetime
+from multiprocessing import Process, Queue
+
 
 GAMMA = 0.99
 MAX_DIST = 0.01
+mode = 'pushing-fixed-cube'
+
 
 def rewards2R(rewards, gamma=0.99):
     R = 0.0
@@ -12,16 +20,33 @@ def rewards2R(rewards, gamma=0.99):
     return list(reversed(Rs))[:-1]
 
 
+def clip_gradient(gradient):
+    norm = np.linalg.norm(gradient)
+    if norm > 40.0:
+        return 40.0 * gradient / norm
+    else:
+        return gradient
+
+
 def trainer(actor_params, critic_params, actor_gradients, critic_gradients):
     import numpy as np
     from a3c import Actor, Critic
     from environment import Environment, WIN, LOSE, NEUTRAL
     
-    env = Environment(MAX_DIST, 'pushing-fixed-goal')
+    np.random.seed(datetime.now().microsecond)
+    
+    episode_length = 0
+    env = Environment(MAX_DIST, mode)
     actor = Actor(env.get_state().shape[1])
     critic = Critic(env.get_state().shape[1])
-
+    actor.load_params('actor_params.txt')
+    critic.load_params('critic_params.txt')
+    
     while True:
+        if episode_length > 32:
+            env.reset()
+            episode_length = 0
+        episode_length += 1
         if not actor_params.empty():
             actor.set_params(actor_params.get())
         if not critic_params.empty():
@@ -31,7 +56,7 @@ def trainer(actor_params, critic_params, actor_gradients, critic_gradients):
         U = []
         x2s = []
         rewards = []
-        for i in range(64):
+        for i in range(5):
             mu, sigma = actor.predict(env.get_state())
             dx, dy = np.random.multivariate_normal(mu[0, :], np.diag(sigma[0, :]))
             X.append(env.get_state().flatten())
@@ -53,17 +78,11 @@ def trainer(actor_params, critic_params, actor_gradients, critic_gradients):
         X = np.array(X)
         U = np.array(U)
         R = np.array([R]).T
-        ga = actor.gradients(X, U, R, V)
-        gc = critic.gradients(X, R)
+        ga = list(map(clip_gradient, actor.gradients(X, U, R, V)))
+        gc = list(map(clip_gradient, critic.gradients(X, R)))
         actor_gradients.put(ga)
         critic_gradients.put(gc)
-
-from time import sleep
-from queue import Full
-import multiprocessing
-from datetime import datetime
-from multiprocessing import Process, Queue
-
+        
 n_processes = 6
 actor_gradient_queue = Queue(maxsize=n_processes)
 critic_gradient_queue = Queue(maxsize=n_processes)
@@ -84,8 +103,12 @@ for _ in range(n_processes):
         )
     )
 
+[p.start() for p in pool]
+sleep(15.0)
+
 
 import numpy as np
+
 from a3c import Actor, Critic
 from environment import Environment, WIN, LOSE, NEUTRAL
 
@@ -105,15 +128,30 @@ def return_average(env, actor, gamma=0.99):
     return np.mean(rewards), np.std(rewards)
 
 
-[p.start() for p in pool]
-averages = []
-sleep(10.0)
+try:
+    with open('averages.txt', 'w') as f:
+        averages = json.loads(f.read())
+except IOError:
+    print('Could not load previous averages')
+    averages = []
 
-env = Environment(MAX_DIST, 'pushing-fixed-goal')
-actor = Actor(env.get_state().shape[1])
-critic = Critic(env.get_state().shape[1])
+env = Environment(MAX_DIST, mode)
+actor = Actor(env.get_state().shape[1], adam_beta1=0.0)
+critic = Critic(env.get_state().shape[1], adam_beta1=0.0)
+try:
+    actor.load_params('actor_params.txt')
+    critic.load_params('critic_params.txt')
+except IOError:
+    print('Could not load previous parameters')
 
-for i in range(2 ** 19):
+
+best_score = -np.inf
+r_avg, r_std = return_average(env, actor)
+averages.append(r_avg)
+if r_avg > best_score:
+    best_score = r_avg
+i = len(averages) * 1024
+for i in range(i, 2 ** 20):
     cg = critic_gradient_queue.get(timeout=1)
     critic.update(cg)
     for c in critic_param_queues:
@@ -128,6 +166,12 @@ for i in range(2 ** 19):
             a.put(actor.params, block=False)
         except Full:
             pass
-    if i % 4096 == 0:
+    if i % 1024 == 0:
         r_avg, r_std = return_average(env, actor)
-        print('Estimated expected return:', r_avg)
+        averages.append(r_avg)
+        if r_avg > best_score:
+            with open('averages.txt', 'w') as f:
+                f.write(json.dumps(averages))
+            actor.save_params('actor_params.txt')
+            critic.save_params('critic_params.txt')
+            best_score = r_avg
